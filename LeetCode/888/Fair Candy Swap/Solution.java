e97812a2076bcdf3d5a97abe36cfa1a695f3115d
ymbcxb <353560278@qq.com>
2019-03-28 07:59:53 +0800

    3.28

---
+++ LeetCode/888/Fair Candy Swap/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] fairCandySwap(int[] A, int[] B) {
+        int sumA = 0;
+        int sumB = 0;
+        for (int i = 0; i < A.length; i++) {
+            sumA = sumA + A[i];
+            sumB = sumB + B[i];
+        }       
+    }
+}
