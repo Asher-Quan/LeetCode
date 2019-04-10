c56daccb4465af31feffc4c034b0fa61732363ac
ymbcxb <353560278@qq.com>
2019-04-10 09:06:37 +0800

    4.10

--- LeetCode/20/Valid Parentheses/Solution.java
+++ LeetCode/20/Valid Parentheses/Solution.java
@@ -1,10 +1,22 @@
+import java.util.Stack;
 
 public class Solution {
 	public static boolean isValid(String s) {
-        boolean flag=false;
-        return flag;
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0 ;i < s.length() ; i++) {
+			if(s.charAt(i) == ')' && !stack.empty() && stack.peek() == '('){
+				stack.pop();
+			}else if(s.charAt(i) == ']' && !stack.empty() && stack.peek() == '['){
+				stack.pop();
+			}else if(s.charAt(i) == '}' && !stack.empty() && stack.peek() == '{'){
+				stack.pop();
+			}else{
+				stack.push(s.charAt(i));
+			}
+		}
+		if(stack.empty())
+			return true;
+		else
+		 	return false;
     }
-	public static void main(String[] args) {
-		System.out.println(isValid("()"));
-	}
 }
--- LeetCode/931/Minimum Falling Path Sum/Solution.java
+++ LeetCode/931/Minimum Falling Path Sum/Solution.java
@@ -1,10 +1,13 @@
 class Solution {
     public int minFallingPathSum(int[][] A) {
+        int [][] dp = new int[A.length][A[0].length];
         if(A == null || A.length == 0)
             return 0;
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[0].length; j++) {
-                
+                if(i == 0)
+                    dp[i][j] = A[i][j];
+                dp[i][j] = Math.min(dp[i-1][j], dp[i-1][j+1]);
             }
         }
     }
