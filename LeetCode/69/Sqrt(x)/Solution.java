01829fcd85b0d79a1d764b43e75bf9ff4393e1e1
ymbcxb <353560278@qq.com>
2019-05-23 13:24:22 +0800

    5.23

--- LeetCode/69/Sqrt(x)/Solution.java
+++ LeetCode/69/Sqrt(x)/Solution.java
@@ -1,5 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        while
+        int left = 1, right = x;            
+    while(left<=right) {
+        int mid = left + ((right-left)>>2);
+        if(mid <= x/mid) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left-1;
     }
 }
