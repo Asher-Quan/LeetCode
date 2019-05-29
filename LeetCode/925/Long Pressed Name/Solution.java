2a3a92998518bc3cbfea0331de0bfa164454fb26
ymbcxb <353560278@qq.com>
2019-05-29 21:47:05 +0800

    5.29

--- LeetCode/925/Long Pressed Name/Solution.java
+++ LeetCode/925/Long Pressed Name/Solution.java
@@ -4,16 +4,19 @@ class Solution {
         int j = 0;
         while( i < name.length() && j < typed.length() ){
             if(name.charAt(i) == typed.charAt(j)){
-                j++;
-            }else if(i+1 < name.length() && name.charAt(i+1) == typed.charAt(j) ){
                 i++;
+                j++;
+            }else if(i>= 1 && name.charAt(i-1) == typed.charAt(j)){
+                j++;
             }else{
                 return false;
             }
         }
-        if( i < name.length() - 1)
+        if(i < name.length() ){
             return false;
-        else
+        }
+        else{
             return true;
+        }
     }
 }
